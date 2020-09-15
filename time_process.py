cdd30322f071c4f8900c59a95856297b134b7ff8
matdexir <matthieudesir21@gmail.com>
2020-09-15 13:11:54 +0800

    Updated Timer

--- time_process.py
+++ time_process.py
@@ -20,8 +20,9 @@ def to_seconds(countdown):
 def countdown(seconds):
     import time
     while seconds:
-        mins, secs = divmod(seconds,60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
+        mins, secs = divmod(seconds, 60)
+        hours, mins = divmod(mins ,60)
+        timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
         print("Killing Spotify in " + timer, end='\r')
         time.sleep(1)
         seconds -= 1
